7334b58c0722e76e499793811f0768e7e0e8b6a3
Dmitry Kashinsky <dmitry.kashinsky@gmail.com>
2019-05-17 17:57:37 +0400

    user component width limit. fixed reference to the data contracts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { UserInfo, ApiDataListResponse, ApiDataResponse } from './user.data';
+import { UserInfo, ApiDataListResponse, ApiDataResponse } from '../user.data';
 
 @Injectable({
   providedIn: 'root'
